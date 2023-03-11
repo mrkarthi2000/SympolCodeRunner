e68dc232cc84ba5a5b54661b8edce055bb433ddf
mrkarthi2000 <103568847+mrkarthi2000@users.noreply.github.com>
2023-03-11 18:31:25 +0530

    Update BasicCode.java

--- BasicCode.java
+++ BasicCode.java
@@ -2,4 +2,11 @@ package com.lao.conditionalstatement;
 
 public class BasicCode {
 
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+		System.out.println("Hello, World1!");
+		System.out.println("Hello, World2!");
+
+	}
+
 }
